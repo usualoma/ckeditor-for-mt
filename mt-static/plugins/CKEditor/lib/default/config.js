a9fba7d77a2bb1d36c13efceb0004d06b8b8bc62
Taku Amano <taku@toi-planning.net>
2009-11-07 16:31:00 +0900

    Renamed config file.

--- mt-static/plugins/CKEditor/lib/default/config.js
+++ mt-static/plugins/CKEditor/lib/default/config.js
@@ -4,4 +4,4 @@ CKEDITOR.config.templates = 'movabletype';
 CKEDITOR.config.templates_files = [
 	StaticURI + '/plugins/CKEditor/template/config.js'
 ];
-CKEDITOR.config.stylesCombo_stylesSet = 'movabletype:' + StaticURI + '/plugins/CKEditor/style.js';
+CKEDITOR.config.stylesCombo_stylesSet = 'movabletype:' + StaticURI + '/plugins/CKEditor/style/config.js';
